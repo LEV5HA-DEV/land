1881f9f3c0449c96b73c8c77b91423d00c04d273
LEV5HA-DEV <149094416+LEV5HA-DEV@users.noreply.github.com>
2024-02-15 00:22:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ document.querySelectorAll(".accordion-header").forEach(header =>
             });
         });
     });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function()
+    {
+        document.querySelector(".header").classList.toggle("open")
+    })
+})
